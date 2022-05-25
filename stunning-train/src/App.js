70ad84e425080cf9ac9db78037ce0dcb509c49fd
HBK <sammcowen@gmail.com>
2022-05-25 15:24:39 -0700

    created components folder. created nav component. renders to the page

---
+++ stunning-train/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+<Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ stunning-train/src/components/Header/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+        {/* <img src="./assets/images/20210616_170521.jpg" /> */}
+        <h1>Samm Cowen</h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#about-me">About Me</a>
+                </li>
+                <li>
+                    <a href="#my-work">My Work</a>
+                </li>
+                <li>
+                    <a href="#contact-me">Contact Me</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    )
+}
+export default Header;
