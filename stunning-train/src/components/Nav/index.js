3ca1811766aa158dcc2ed5d269c61da7ff2407f1
HBK <sammcowen@gmail.com>
2022-05-27 12:35:25 -0700

    condition rendering for nav tabs

--- stunning-train/src/components/Nav/index.js
+++ stunning-train/src/components/Nav/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 
-function Nav() {
+function Nav(props) {
+    const tabs = ['About Me', 'Portfolio', 'Contact Me'];
     return (
-        <nav>
-        <ul>
-            <li>
+        <div>
+           <nav>
+                <ul>
+                    {tabs.map((tab) => (
+            <li className={props.currentPage === tab ? 'nav-item is-active' : 'nav-item'} key={tab}>
                 <a href="#about-me">About Me</a>
-            </li>
-            <li>
-                <a href="#my-work">My Work</a>
-            </li>
-            <li>
-                <a href="#contact-me">Contact Me</a>
-            </li>
-        </ul>
-        </nav> 
+            
+            
+                <a href={"#" + tab.toLowerCase()} onClick={() => props.handlePageChange(tab)} className={props.currentPage === tab ? 'nav-link active' : 'nav-link'}>
+                    {tab}
+           </a>
+         </li>
+                    ))}
+                </ul>
+           </nav>
+        </div>
     );
 };
 
-export default  Nav;
+export default Nav;
