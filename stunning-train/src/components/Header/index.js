1f75cd806ef60c8cbaa2bfbb0fd7f08e2ec3c91c
HBK <sammcowen@gmail.com>
2022-05-27 12:45:36 -0700

    got image to properly render in the browser

--- stunning-train/src/components/Header/index.js
+++ stunning-train/src/components/Header/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Nav from "../Nav";
-// import Image from '../../assets/images';
+import Image from '../../assets/images/20210616_170521.jpg';
 
 
 function Header() {
     return (
         <header>
-         {/* <img  src={Image} alt = "samm cowen "/> */}
+         <img  src={Image} alt = "samm cowen "/>
        
         <h1>Samm Cowen</h1>
 
--- stunning-train/src/components/Nav/index.js
+++ stunning-train/src/components/Nav/index.js
@@ -1,25 +1,45 @@
 import React from 'react';
 
-function Nav(props) {
-    const tabs = ['About Me', 'Portfolio', 'Contact Me'];
+// function Nav(props) {
+//     const tabs = ['About Me', 'Portfolio', 'Contact Me'];
+//     return (
+//         <div>
+//            <nav>
+//                 <ul>
+//                     {tabs.map((tab) => (
+//             <li className={props.currentPage === tab ? 'nav-item is-active' : 'nav-item'} key={tab}>
+//                 <a href="#about-me">About Me</a>
+            
+            
+//                 <a href={"#" + tab.toLowerCase()} onClick={() => props.handlePageChange(tab)} className={props.currentPage === tab ? 'nav-link active' : 'nav-link'}>
+//                     {tab}
+//            </a>
+//          </li>
+//                     ))}
+//                 </ul>
+//            </nav>
+//         </div>
+//     );
+// };
+function Nav () {
     return (
         <div>
-           <nav>
-                <ul>
-                    {tabs.map((tab) => (
-            <li className={props.currentPage === tab ? 'nav-item is-active' : 'nav-item'} key={tab}>
-                <a href="#about-me">About Me</a>
-            
-            
-                <a href={"#" + tab.toLowerCase()} onClick={() => props.handlePageChange(tab)} className={props.currentPage === tab ? 'nav-link active' : 'nav-link'}>
-                    {tab}
-           </a>
-         </li>
-                    ))}
-                </ul>
-           </nav>
+            <nav>
+            <ul>
+                <li>
+                    <a href="#about-me">About Me</a>
+                </li>
+                <li>
+                    <a href="#my-work">My Work</a>
+                </li>
+                <li>
+                    <a href="#contact-me">Contact Me</a>
+                </li>
+            </ul>
+        </nav>
         </div>
-    );
-};
+    )
+
+}
 
 export default Nav;
