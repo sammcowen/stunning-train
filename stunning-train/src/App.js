dfcc5cefe7be928f3a94f489d99f7a0c7aaa2ed5
HBK <sammcowen@gmail.com>
2022-05-28 10:28:33 -0700

    added images for my work section.

--- stunning-train/src/App.js
+++ stunning-train/src/App.js
@@ -2,17 +2,18 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
+import Project from './components/Project';
 
-import './App.css';
+import './index.css';
 
 function App() {
   return (
    <div>
 <Header>
   <Nav>
-
-  </Nav>
+    </Nav>
 </Header>
+<Project></Project>
 
 
 <Footer></Footer>
--- stunning-train/src/components/Project/index.js
+++ stunning-train/src/components/Project/index.js
@@ -1,6 +1,64 @@
-// import React from 'react';
-// function Project(props) {
-//     return (
+import React from 'react';
 
-//     )
-// }
+import Only from '../../assets/images/onlyfriends.jpg';
+import Weather from '../../assets/images/weather.jpg';
+import Notepad from '../../assets/images/notepad.png';
+import Crypto from '../../assets/images/crypto.jpg';
+import Scheduler from '../../assets/images/scheduler.jpg';
+
+function Project() {
+    return (
+        <div>
+<div class="row title">
+            <div id="my-work" class="col col-4">
+                <h2>Work</h2>
+            </div>
+            <div class="grid col col-8">
+                <div id="onlyfriends" class="card">
+                    <img src={Only} class="card-img-top"/>
+                    <div class="card-body">
+                        <a href="https://only-friendz.herokuapp.com" target="_blank" class="btn col-12 btn-primary">Only_Friends<br/>
+                            Node.js/Express/Handlebars</a>
+                    </div>
+                </div>
+                
+                <div id="weather" class="card">
+                    <img src={Weather} class="card-img-top"/>
+                    <div class="card-body">
+                        <a href="https://sammcowen.github.io/weather-dashboard/" target= "_blank"class="btn col-12 btn-primary">Weather
+                            Dashboard <br/> JS/Jquery/CSS</a>
+                    </div>
+                </div>
+
+                <div id="notepad" class="card">
+                    <img src={Notepad} class="card-img-top"/>
+                    <div class="card-body">
+                        <a href="https://sammcowen.github.io/NotePad/"target="_blank"
+                            class="btn col-12 btn-primary">NotePad<br/>
+                            Express.js/JS/CSS</a>
+                    </div>
+                </div>
+
+                <div id="crypto" class="card">
+                    <img src={Crypto} class="card-img-top"/>
+                    <div class="card-body">
+                        <a href="https://themanwiththeplan-eng.github.io/cryptGo/"target="_blank"
+                            class="btn col-12 btn-primary">cryptGo<br/>
+                            JS/Jquery/CSS</a>
+                    </div>
+                </div>
+                <div id="workday" class="card">
+                    <img src={Scheduler} class="card-img-top"/>
+                    <div class="card-body">
+                        <a href="https://sammcowen.github.io/work-day-scheduler/" target="_blank" class="btn col-12 btn-primary">Work
+                            Day
+                            Scheduler<br/>HTML/CSS<br/> JS/Jquery</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default Project;
